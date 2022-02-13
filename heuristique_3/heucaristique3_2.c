8fddc61bf5422b497bb8fd76b99965a26ba1250d
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-02-13 20:16:26 +0100

    3.2 clear

--- heuristique_3/heucaristique3_2.c
+++ heuristique_3/heucaristique3_2.c
@@ -5,9 +5,13 @@
 #include "../Programme_Fourni/affichage.c"
 #include "../Programme_Fourni/lectureFichier.c"
 
+#define ITEMS_IN_ARRAY(array)   (sizeof(array) / sizeof(*array))
+
 // recherche la ville la plus proche de la ville "num_ville"
 int recherche_voisin_proche(Graphe g, int n, int num_ville, int liste_ville[]);
 
+// calcul du poids total d'un chemin en fcontion d'un graphe, du nb de sommet et de l'ordre des sommets traversé
+int calcul_poids_total(Graphe g, int n, int ordre_ville[]);
 
 int main(int argc, char const *argv[])
 {
@@ -27,25 +31,66 @@ int main(int argc, char const *argv[])
 		err = lire_data(nom, &G, &n, &m);
 	}while(err == 0);
 
-	int liste_ville[n];
+	/*
+	Liste_ville sert a verifier les villes dans lesquelles on est deja passé
+	ordre_ville sert a sauvegarder l'ordre dans lequel on est passé
+	*/
+	int *liste_ville = NULL, *ordre_ville = NULL;
+	
+
+	liste_ville = malloc(n * sizeof(int));
+	ordre_ville = malloc(n * sizeof(int));
+	printf("Here");
+
 	liste_ville[ville_actuelle] = 1;
+	ordre_ville[0] = ville_actuelle;
 
       //affichage
-      affiche_km(G, n);
+      //affiche_km(G, n);
 
 
 	printf("ville ou je suis : %d\n", ville_actuelle);
 	for(int i = 0; i < n; i++)
 	{
+		/*
+		//Bug apparait a ce moment la
+		if (i < 10)
+		{
+			printf("\nI de la premiere boucle : %d\n", i);
+			
+			int total = 200;
+			for (int i = 0; i < n; i++)
+			{
+				if (liste_ville[i] != 1 )
+				{
+					total -= 1;
+					
+				}
+				else{
+					printf("Passage a i = %d\n", i);
+				}
+				
+			}			
+		}
+		*/
+		
 		ville_suivante = recherche_voisin_proche(G, n, ville_actuelle, liste_ville);
+		
+		//On rentre ici seulement si l'on a pas exploré toutes les villes
 		if (ville_suivante != -1)
 		{
 			ville_actuelle = ville_suivante;
 			liste_ville[ville_actuelle] = 1;
+			ordre_ville[i+1] = ville_actuelle;;
 			printf("ville ou je suis : %d\n", ville_actuelle);
 		}
-		
 	}
+	
+	// Calcul distance totale
+	int distance_totale = calcul_poids_total(G, n, ordre_ville);
+
+	printf("\nLe distance totale est de %4d km", distance_totale);
+	
       return 0;
 }
 
@@ -68,4 +113,24 @@ int recherche_voisin_proche(Graphe g, int n, int num_ville, int liste_ville[]){
 		}
 	}
 	return num_voisin_proche;
+}
+
+int calcul_poids_total(Graphe g, int n, int ordre_ville[]){
+	int somme_poids = 0;
+	int point_a, point_b;
+
+	for (int i = 0; i < n-1; i++)
+	{
+		point_a = ordre_ville[i];
+		point_b = ordre_ville[i+1];
+				
+		somme_poids += g[point_a][point_b];
+		
+		/* Debug
+		printf("\nPoint a : %d, Point b : %d", point_a, point_b);
+		printf("\nPoids actuel : %d", somme_poids);
+		*/
+	}
+
+	return somme_poids;
 }
