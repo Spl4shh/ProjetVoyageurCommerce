254b74ad6afe0f50c849b59b7490ae0a5049a3c4
LothaireNobili <91308048+LothaireNobili@users.noreply.github.com>
2022-02-13 16:07:02 +0100

    edit import
    import des fichier donnée fait, suppression des fonctions inutiles

--- heuristique_3/eucharistique3_1.c
+++ heuristique_3/eucharistique3_1.c
@@ -1,25 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <time.h>
-
-
-
-// définition du type Graphe comme un tableau à 2 dimensions
-// allocation dynamique faite au moment de la lecture des
-// données dans un fichier
-typedef int ** Graphe;
-
-
-// lecture des données dans un fichier et construction du graphe
-int lire_data(char * nom, Graphe * g, int *n, int *m);
-
-
-// affichage du graphe, i.e. du tableau des distances
-void affiche_km(int ** g, int n);
-
-
-// affichage de la distance entre la ville en question et les autre villes
-void affiche_une_ville(int ** g, int n, int num_ville);
+#include "../Programme_Fourni/graphe.h"
+#include "../Programme_Fourni/affichage.c"
+#include "../Programme_Fourni/lectureFichier.c"
 
 
 int main(int argc, char const *argv[]){
@@ -53,8 +37,6 @@ int main(int argc, char const *argv[]){
 }
 
 
-
-
 isNotIn(int r, int *p, int l){
     for (int i=0; i<l; i++){
         if (r==p[i]){
@@ -62,61 +44,4 @@ isNotIn(int r, int *p, int l){
         }
     }
     return 1;
-}
-
-int lire_data(char * nom, Graphe * g, int *nb, int *m)
-{
-	char str[15];
-	int i, s1, s2, km;
-	FILE * f = fopen(nom, "r");
-	
-	if (f == NULL) return 0; // le fichier n'existe pas
-	
-	// la première ligne contient le nombre de sommets n et le nombre d'arêtes m
-	fgets(str, 15, f);
-	sscanf(str, "%d %d", nb, m);
-	
-	// allocation dynamique d'un tableau n x n rempli de 0
-	*g = (int **)calloc(sizeof(int *), *nb);
-	for (i = 0; i < *nb; i++){
-		(*g)[i] = (int *)calloc(sizeof(int), *nb);
-	} 
-
-	// lecture du fichier et remplissage du tableau G
-	for (i = 0; i < *m; i++)
-	{
-		fgets(str, 15, f);
-		sscanf(str, "%d %d %d", &s1, &s2, &km);
-		(*g)[s1][s2] = km;
-		(*g)[s2][s1] = km;
-	}
-	fclose(f);
-	return 1;
-}
-
-void affiche_km(Graphe g, int n)
-{
-	int i, j;
-	
-	int ville_choisie = 5;
-
-	for(i = 0; i < n; i++)
-	{
-		for(j = 0; j < n; j++) {
-			printf("%5d ", g[i][j]);
-		}
-		printf("\n");
-	}		
-	printf("\n");
-}
-
-void affiche_une_ville(Graphe g, int n, int num_ville)
-{
-	int i;
-	
-	for(i = 0; i < n; i++)
-	{
-		printf("%5d ", g[num_ville][i]);
-	}
-	printf("\n");
 }
