a47ed403532072574b5174d37a3fb626ce6d2cc4
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-02-13 17:14:45 +0100

    Update heucaristique3_2.c

--- heuristique_3/heucaristique3_2.c
+++ heuristique_3/heucaristique3_2.c
@@ -1,22 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// définition du type Graphe comme un tableau à 2 dimensions
-// allocation dynamique faite au moment de la lecture des
-// données dans un fichier
-typedef int ** Graphe;
-
-// lecture des données dans un fichier et construction du graphe
-int lire_data(char * nom, Graphe * g, int *n, int *m);
-
-// affichage du graphe, i.e. du tableau des distances
-void affiche_km(int ** g, int n);
-
-// affichage de la distance entre la ville en question et les autre villes
-void affiche_une_ville(int ** g, int n, int num_ville);
+#include "../Programme_Fourni/graphe.h"
+#include "../Programme_Fourni/affichage.c"
+#include "../Programme_Fourni/lectureFichier.c"
 
 // recherche la ville la plus proche de la ville "num_ville"
 int recherche_voisin_proche(Graphe g, int n, int num_ville, int liste_ville[]);
+
 
 int main(int argc, char const *argv[])
 {
@@ -25,7 +16,7 @@ int main(int argc, char const *argv[])
 	Graphe G = NULL;
 	int n, m;
 	int err;
-	int ville_actuelle = 0;
+	int ville_actuelle = 0, ville_suivante;
       //End Variable
 
       //lecture du fichier
@@ -43,65 +34,19 @@ int main(int argc, char const *argv[])
       affiche_km(G, n);
 
 
+	printf("ville ou je suis : %d\n", ville_actuelle);
 	for(int i = 0; i < n; i++)
 	{
-		ville_actuelle = recherche_voisin_proche(G, n, ville_actuelle, liste_ville);
-
-	}
-
-      return 0;
-}
-
-int lire_data(char * nom, Graphe * g, int *n, int *m){
-	char str[15];
-	int i, s1, s2, km;
-	FILE * f = fopen(nom, "r");
-	
-	if (f == NULL) return 0; // le fichier n'existe pas
-	
-	// la première ligne contient le nombre de sommets n et le nombre d'arêtes m
-	fgets(str, 15, f);
-	sscanf(str, "%d %d", n, m);
-	
-	// allocation dynamique d'un tableau n x n rempli de 0
-	*g = (int **)calloc(sizeof(int *), *n);
-	for (i = 0; i < *n; i++) (*g)[i] = (int *)calloc(sizeof(int), *n);
-
-	// lecture du fichier et remplissage du tableau G
-	for (i = 0; i < *m; i++)
-	{
-		fgets(str, 15, f);
-		sscanf(str, "%d %d %d", &s1, &s2, &km);
-		(*g)[s1][s2] = km;
-		(*g)[s2][s1] = km;
-	}
-	fclose(f);
-	return 1;
-}
-
-void affiche_km(Graphe g, int n){
-	int i, j;
-	
-	int ville_choisie = 5;
-
-	for(i = 0; i < n; i++)
-	{
-		for(j = 0; j < n; j++) {
-			printf("%5d ", g[i][j]);
+		ville_suivante = recherche_voisin_proche(G, n, ville_actuelle, liste_ville);
+		if (ville_suivante != -1)
+		{
+			ville_actuelle = ville_suivante;
+			liste_ville[ville_actuelle] = 1;
+			printf("ville ou je suis : %d\n", ville_actuelle);
 		}
-		printf("\n");
-	}		
-	printf("\n");
-}
-
-void affiche_une_ville(Graphe g, int n, int num_ville){
-	int i;
-	
-	for(i = 0; i < n; i++)
-	{
-		printf("%5d ", g[num_ville][i]);
+		
 	}
-	printf("\n");
+      return 0;
 }
 
 int recherche_voisin_proche(Graphe g, int n, int num_ville, int liste_ville[]){
@@ -123,5 +68,4 @@ int recherche_voisin_proche(Graphe g, int n, int num_ville, int liste_ville[]){
 		}
 	}
 	return num_voisin_proche;
-	//printf("Le num de la ville la plus proche est : %d a une distance de : %5dkm \n", num_voisin_proche, g[num_ville][num_voisin_proche]);
 }
