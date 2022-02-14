dbc57c36f3aa79a30ab1a0872f7d80c2e62cb553
LothaireNobili <lothairenobili@gmail.com>
2022-02-14 19:36:52 +0100

    correction diverse 3-1
    Rework complet du 3-1 en utilisant les fonctions fournisent par le prof
    le programme teste maintenant plusieurs chemin et garde le meilleur

--- heuristique_3/eucharistique3_1.c
+++ heuristique_3/eucharistique3_1.c
@@ -2,9 +2,32 @@
 #include<stdlib.h>
 #include <time.h>
 #include "../Programme_Fourni/graphe.h"
-#include "../Programme_Fourni/affichage.h"
-#include "../Programme_Fourni/lectureFichier.h"
+#include "../Programme_Fourni/affichage.c"
+#include "../Programme_Fourni/lectureFichier.c"
+#include "../Programme_Fourni/permutation.c"
 
+
+int get_poids_total(Graphe g, int n, int *ordre_ville){
+	int somme_poids = 0;
+	int point_a, point_b;
+
+	for (int i = 0; i < n-1; i++)
+	{
+		point_a = ordre_ville[i];
+		point_b = ordre_ville[i+1];
+				
+		somme_poids += g[point_a][point_b];
+
+	}
+
+	return somme_poids;
+}
+
+void setTable(int *a, int *b, int n){
+    for (int i=0; i<n; i++){
+        a[i]=b[i];
+    }
+}
 
 
 int isNotIn(int r, int *p, int l){
@@ -21,6 +44,7 @@ int main(int argc, char const *argv[]){
     Graphe g = NULL;
     int n, m;
     int err;
+    int poid;
     
     do{
 		printf("saisir le nom de fichier de donnees : ");
@@ -29,25 +53,31 @@ int main(int argc, char const *argv[]){
 		err = lire_data(nom, &g, &n, &m);
 	}while(err == 0);
    
+   
    int liste_ville[n];
    for (int i=0; i<n; i++){
-       liste_ville[n]=-1;
+       liste_ville[n]=i;
    }
+   printf("après création liste ville\n");
+   
+   int poids = get_poids_total(g, n, liste_ville);
+   printf("après 1er get poids liste ville\n");
 
-   srand(time(NULL));
-   int r;
-
-   for(int i=0; i<n; i++){
-       while ((isNotIn (r, liste_ville, n))==0){
-           r = rand()%n;
+   int meilleur_chemin[n];
+   setTable(meilleur_chemin, liste_ville, n);
+    printf("avant boucle");
+   for(int i=0; i<10; i++){
+       permut_complete(liste_ville, n);
+       if(get_poids_total(g, n, liste_ville)<(get_poids_total(g, n, meilleur_chemin))){
+           setTable(meilleur_chemin, liste_ville, n);
        }
-       liste_ville[i]=r;
+       printf("boucle n %d", i);
    }
 
 
-   for(int i=0; i<n; i++){
-       printf("%d ", liste_ville[i]);
-   }
-
+   
+   printf("Listes des villes : ");
+   affiche_tableau(meilleur_chemin, n);
+   printf("\nPoid du voyage : %d ", get_poids_total(g, n, meilleur_chemin));
 }
 
