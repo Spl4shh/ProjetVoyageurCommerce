2b57b068e4ded68427e6f1be5a7ea32397467004
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-02-14 20:53:59 +0100

    Update heuristique4_2.c

--- heuristique_4/heuristique4_2.c
+++ heuristique_4/heuristique4_2.c
@@ -11,10 +11,17 @@
 	Pour l'heuristique 4.2, on reutilise l'heucaristique 3.2 et on créé les fonctions nécéssaires
 */
 
-/* permutter 2 points avec un deplacement afin de (dé)croiser une arrete
-   le point a correspond a depart de la premiere arrete et le point b correspond au depart de la deuxieme arrete
+/* Permutter 2 points avec un deplacement afin de (dé)croiser une arrete
+   Le point a correspond a depart de la premiere arrete et le point b correspond au depart de la deuxieme arrete
+   Conditions : 
+	point_a >= 0 
+	point_a < n-1
+	point_b < n-1
+	point_a < point_b
+	point_a - point_b > 1
+   Avec n la taille du tableau
 */
-void croiserPoint(int point_a, int point_b, int n, int ordre_ville[]);
+int* croiserPoint(int point_a, int point_b, int n, int ordre_ville[]);
 
 int main(int argc, char const *argv[])
 {
@@ -72,12 +79,31 @@ int main(int argc, char const *argv[])
 	}
 	printf("\nLe distance totale est de %4d km", distance_totale);
 	
+	int *ordre_ville1 = croiserPoint(1, 3, n, ordre_ville);
+	printf("\nL'ordre de visite est : ");
+	for (int i = 0; i < n; i++)
+	{
+		printf(" -> %d", ordre_ville1[i]);
+	}
+	int distance_g1 = getPoidsTotal(G, n, ordre_ville1);
+	printf("\nLe distance totale est de %4d km", distance_g1);
+
       return 0;
 }
 
-void croiserPoint(int point_a, int point_b, int n, int ordre_ville[]){
-	int tempo;
+int* croiserPoint(int point_a, int point_b, int n, int ordre_ville[]){
+	if (point_a >= 0 && point_a < n-1 && point_b < n-1 && point_a < point_b && (point_a - point_b) > 1)
+	{ 
+		int tempo;
 
-	tempo = ordre_ville[point_a + 1];
-	ordre_ville[point_a + 1] = ordre_ville[point_b + 1];
+		tempo = ordre_ville[point_a + 1];
+		ordre_ville[point_a + 1] = ordre_ville[point_b + 1];
+		ordre_ville[point_b + 1] = ordre_ville[point_b];
+		ordre_ville[point_b] = tempo;
+
+		return ordre_ville;
+	}else{
+		printf("Les conditions d'utilisation de la fonction ne sont pas respecte, Merci de verifier le code.\nTableau d'origine retourne\n");
+		return(ordre_ville);
+	}	
 }
