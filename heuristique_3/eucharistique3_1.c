dafc4d95bf746590fe41a146cd7514b2f9ea2853
LothaireNobili <91308048+LothaireNobili@users.noreply.github.com>
2022-02-14 20:32:16 +0100

    edit vériée
    rework du pgrm en prenant en compte les fonction donné elapr le prof
    le prof essaie deseromais puseirus trajet ét garde le meileur en mémoire

--- heuristique_3/eucharistique3_1.c
+++ heuristique_3/eucharistique3_1.c
@@ -4,45 +4,33 @@
 #include "../Programme_Fourni/graphe.h"
 #include "../Programme_Fourni/affichage.c"
 #include "../Programme_Fourni/lectureFichier.c"
+#include "../Programme_Fourni/permutation.c"
 
 
-int main(int argc, char const *argv[]){
-    char nom[30];
-    Graphe g = NULL;
-    int n, m;
-    int err;
-    
-    do{
-		printf("saisir le nom de fichier de donnees : ");
-		scanf("%s", nom); 
-		while(getchar() != '\n');
-		err = lire_data(nom, &G, &n, &m);
-	}while(err == 0);
-   
-   int liste_ville[n];
-   for (int i=0; i<n; i++){
-       liste_ville[n]=-1;
-   }
+int get_poids_total(Graphe g, int n, int *ordre_ville){
+	int somme_poids = 0;
+	int point_a, point_b;
 
-   srand(time(NULL));
-   int r;
+	for (int i = 0; i < n-1; i++)
+	{
+		point_a = ordre_ville[i];
+		point_b = ordre_ville[i+1];
+				
+		somme_poids += g[point_a][point_b];
 
-   for(i=0; i<n; i++){
-       while ((isNotIn (r, liste_ville, n))==0){
-           r = rand()%n
-       }
-       liste_ville[i]=r;
-   }
+	}
 
+	return somme_poids;
+}
 
-   for(i=0; i<n; i++){
-       printf("%d ", liste_ville[i]);
-   }
-
+void setTable(int *a, int *b, int n){
+    for (int i=0; i<n; i++){
+        a[i]=b[i];
+    }
 }
 
 
-isNotIn(int r, int *p, int l){
+int isNotIn(int r, int *p, int l){
     for (int i=0; i<l; i++){
         if (r==p[i]){
             return 0;
@@ -50,3 +38,46 @@ isNotIn(int r, int *p, int l){
     }
     return 1;
 }
+
+int main(int argc, char const *argv[]){
+    char nom[30];
+    Graphe g = NULL;
+    int n, m;
+    int err;
+    int poid;
+    
+    do{
+		printf("saisir le nom de fichier de donnees : ");
+		scanf("%s", nom); 
+		while(getchar() != '\n');
+		err = lire_data(nom, &g, &n, &m);
+	}while(err == 0);
+   
+   
+   int liste_ville[n];
+   for (int i=0; i<n; i++){
+       liste_ville[n]=i;
+   }
+   printf("après création liste ville\n");
+   
+   int poids = get_poids_total(g, n, liste_ville);
+   printf("après 1er get poids liste ville\n");
+
+   int meilleur_chemin[n];
+   setTable(meilleur_chemin, liste_ville, n);
+    printf("avant boucle");
+   for(int i=0; i<10; i++){
+       permut_complete(liste_ville, n);
+       if(get_poids_total(g, n, liste_ville)<(get_poids_total(g, n, meilleur_chemin))){
+           setTable(meilleur_chemin, liste_ville, n);
+       }
+       printf("boucle n %d", i);
+   }
+
+
+   
+   printf("Listes des villes : ");
+   affiche_tableau(meilleur_chemin, n);
+   printf("\nPoid du voyage : %d ", get_poids_total(g, n, meilleur_chemin));
+}
+
