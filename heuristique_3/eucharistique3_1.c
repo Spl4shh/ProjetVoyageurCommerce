1edc5a2baad63ea7d2bf98e6aae8563146b74c10
LothaireNobili <lothairenobili@gmail.com>
2022-02-14 16:57:55 +0100

    fix compil
    correction des erreurs de compilation

--- heuristique_3/eucharistique3_1.c
+++ heuristique_3/eucharistique3_1.c
@@ -2,9 +2,19 @@
 #include<stdlib.h>
 #include <time.h>
 #include "../Programme_Fourni/graphe.h"
-#include "../Programme_Fourni/affichage.c"
-#include "../Programme_Fourni/lectureFichier.c"
+#include "../Programme_Fourni/affichage.h"
+#include "../Programme_Fourni/lectureFichier.h"
 
+
+
+int isNotIn(int r, int *p, int l){
+    for (int i=0; i<l; i++){
+        if (r==p[i]){
+            return 0;
+        }
+    }
+    return 1;
+}
 
 int main(int argc, char const *argv[]){
     char nom[30];
@@ -16,7 +26,7 @@ int main(int argc, char const *argv[]){
 		printf("saisir le nom de fichier de donnees : ");
 		scanf("%s", nom); 
 		while(getchar() != '\n');
-		err = lire_data(nom, &G, &n, &m);
+		err = lire_data(nom, &g, &n, &m);
 	}while(err == 0);
    
    int liste_ville[n];
@@ -27,26 +37,17 @@ int main(int argc, char const *argv[]){
    srand(time(NULL));
    int r;
 
-   for(i=0; i<n; i++){
+   for(int i=0; i<n; i++){
        while ((isNotIn (r, liste_ville, n))==0){
-           r = rand()%n
+           r = rand()%n;
        }
        liste_ville[i]=r;
    }
 
 
-   for(i=0; i<n; i++){
+   for(int i=0; i<n; i++){
        printf("%d ", liste_ville[i]);
    }
 
 }
 
-
-isNotIn(int r, int *p, int l){
-    for (int i=0; i<l; i++){
-        if (r==p[i]){
-            return 0;
-        }
-    }
-    return 1;
-}
