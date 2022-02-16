14f48aa53bf7c9a8a68f6bd4e536476f6a0a54dd
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-02-16 17:19:26 +0100

    renamme

---
+++ Fonction/affichageCombine.c
@@ -0,0 +1,14 @@
+#ifndef AFFICHAGECOMBINE_C
+	#define AFFICHAGECOMBINE_C
+
+	#include "../Programme_Fourni/graphe.h"
+
+	void afficheCheminPoids(Graphe G, int n, int ordre_ville[]){
+		printf("\nL'ordre de visite est : ");
+		for (int i = 0; i < n; i++){
+			printf(" -> %d", ordre_ville[i]);
+		}
+		printf("\nLe distance totale est de %4d km", getPoidsTotal(G, n, ordre_ville));
+	}
+
+#endif
--- Fonction/affichageCombine.h
+++ Fonction/affichageCombine.h
@@ -1,5 +1,5 @@
-#ifndef AFFICHAGE_H
-      #define AFFICHAGE_H
+#ifndef AFFICHAGECOMBINE_H
+      #define AFFICHAGECOMBINE_H
       #include "../Programme_Fourni/graphe.h"
 
       // affichage du chemin en parametre ainsi que son poids total
