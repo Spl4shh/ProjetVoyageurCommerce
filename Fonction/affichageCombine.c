c1d2528710aa765c65bbbe4bff59bdd2b65d68d7
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-02-16 17:30:22 +0100

    ajout fonction afficheTimer

--- Fonction/affichageCombine.c
+++ Fonction/affichageCombine.c
@@ -3,12 +3,18 @@
 
 	#include "../Programme_Fourni/graphe.h"
 
+      // Affichage du chemin en parametre ainsi que son poids total
 	void afficheCheminPoids(Graphe G, int n, int ordre_ville[]){
 		printf("\nL'ordre de visite est : ");
 		for (int i = 0; i < n; i++){
 			printf(" -> %d", ordre_ville[i]);
 		}
 		printf("\nLe distance totale est de %4d km", getPoidsTotal(G, n, ordre_ville));
+	}
+
+	// Affiche le temps écoulé depuis le lancement de timer
+      void affichageTimer(clock_t timer){
+		printf("\nDuree du calcul : %f secondes", getTempsEcoule(timer));
 	}
 
 #endif
--- Fonction/affichageCombine.h
+++ Fonction/affichageCombine.h
@@ -1,8 +1,12 @@
 #ifndef AFFICHAGECOMBINE_H
       #define AFFICHAGECOMBINE_H
+      
       #include "../Programme_Fourni/graphe.h"
+      #include <time.h>
 
-      // affichage du chemin en parametre ainsi que son poids total
+      // Affichage du chemin en parametre ainsi que son poids total
       void afficheCheminPoids(Graphe G, int n, int ordre_ville[]);
 
+      // Affiche le temps écoulé depuis le lancement de timer
+      void affichageTimer(clock_t timer);
 #endif
