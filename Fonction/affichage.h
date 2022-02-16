d16be7266eff52b35c301174a925393a440cb82d
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-02-16 17:16:21 +0100

    Fonction affichage personnalisé
    Vous pouvez en ajouter d'autre si vous cassez pas tout

---
+++ Fonction/affichage.h
@@ -0,0 +1,8 @@
+#ifndef AFFICHAGE_H
+      #define AFFICHAGE_H
+      #include "../Programme_Fourni/graphe.h"
+
+      // affichage du chemin en parametre ainsi que son poids total
+      void afficheCheminPoids(Graphe G, int n, int ordre_ville[]);
+
+#endif
