7c77a6ec3c5da9129b3ee9bb6846b12bad5187f5
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-02-17 11:20:08 +0100

    table

---
+++ Fonction/table.c
@@ -0,0 +1,14 @@
+#ifndef TABLE_C
+#define TABLE_C
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Copie le tableau B dans le tableau A, de taile size
+void copieTable(int a[], int b[], int size){
+    for (int i = 0; i < size; i++){
+        a[i] = b[i];
+    }
+}
+
+#endif
---
+++ Fonction/table.h
@@ -0,0 +1,6 @@
+#ifndef TABLE_H
+      #define TABLE_H
+
+      // Copie une table dasn une autre
+      void copieTable(int a[], int b[], int size);
+#endif
