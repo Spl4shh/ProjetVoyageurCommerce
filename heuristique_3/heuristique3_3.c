d31b27b32f669d758dd5d0d932ed44eab2898cc4
LothaireNobili <lothairenobili@gmail.com>
2022-02-18 00:33:41 +0100

    création de l'heuristique 3.3
    euristique 3.3 :
    insert les ville dans la liste une par une, garde à chaque insertion le circuit le plus rapide
    en l'état, ce programme fonctionne avec un timer, mais il est possible de le modifier pour appeler la fonction de trie une seule fois, pour de meilleurs performance

---
+++ heuristique_3/heuristique3_3.c
@@ -0,0 +1,153 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <time.h>
+
+#include "../Programme_Fourni/graphe.h"
+#include "../Programme_Fourni/affichage.c"
+#include "../Programme_Fourni/lectureFichier.c"
+#include "../Programme_Fourni/permutation.c"
+#include "../Programme_Fourni/calculTemps.c"
+
+#include "../Fonction/affichageCombine.c"
+#include "../Fonction/table.c"
+#include "../Fonction/poids.c"
+
+#define TIMER_LIMIT 2.0
+
+
+//décale les élément d'une table vers la gauche
+void decalageVersGauche(int *table, int n);
+
+//insert un élément qui viens de la droite de la table
+void insererElementDroite(int decalage, int rangDep, int *liste);
+
+//appel cheminParInsertion en boucle pendant 2 seconde avec des liste différentes, variant aléatoirement
+void rechercheCheminInsertion(Graphe g, int n, int *liste_ville, clock_t timer);
+
+//cherche un chemin rapide en inserant de nouvelle ville une par une, en vérifiant quelle insertion est la plus rapide
+void cheminParInsertion(Graphe g, int n, int *liste_ville);
+
+//retourne le poid total du circuit choisis, 
+int get_poids_total(Graphe g, int n, int *ordre_ville);
+
+
+
+int main(int argc, char const *argv[]){
+    char nom[30];
+    Graphe g = NULL;
+    int n, m;
+    int err;
+
+    clock_t t1;
+	double cpu_boucle;
+
+    do{
+		printf("saisir le nom de fichier de donnees : ");
+		scanf("%s", nom); 
+		while(getchar() != '\n');
+		err = lire_data(nom, &g, &n, &m);
+	}while(err == 0);
+   
+    int liste_ville[n];
+    for (int i = 0; i < n; i++){
+        liste_ville[i] = i;
+    }
+   
+    t1 = clock();
+
+    // Fait des recherches pendant TIMER_LIMIT
+    //cheminParInsertion(g, n, liste_ville, t1);
+    rechercheCheminInsertion(g, n, liste_ville, t1);
+
+    // Affichage
+    affichageTimer(t1);
+    afficheCheminPoids(g, n, liste_ville);
+    
+}
+
+
+void rechercheCheminInsertion(Graphe g, int n, int *liste_ville, clock_t timer){
+    int chemin_test[n];
+    copieTable(chemin_test, liste_ville, n);
+    
+
+    while (getTempsEcoule(timer) < TIMER_LIMIT){
+        permut_complete(chemin_test, n);
+        cheminParInsertion(g, n, liste_ville);
+        if(get_poids_total(g, n, liste_ville) > (get_poids_total(g, n, chemin_test))){
+            copieTable(liste_ville, chemin_test, n);
+        } 
+    }
+}
+    
+
+
+void cheminParInsertion(Graphe g, int n, int *liste_ville){
+    //int checked=2;
+    int chemin_test[n];
+    copieTable(chemin_test, liste_ville, n);
+    //while (getTempsEcoule(timer) < TIMER_LIMIT){
+        //for(int i=0; i<n; i++){
+        for(int checked=2; checked<n-1; checked++){
+            for(int j=0; j<checked; j++){
+                //insererDansTable(j, i, n, liste_ville)
+                insererElementDroite(1, checked-j, chemin_test);
+                /*
+                printf("Poids actuel du chemin sauvegarde : %d \n", get_poids_total(g, checked, liste_ville) );
+                printf("Poids actuel du chemin teste : %d\n", get_poids_total(g, checked, chemin_test));
+                */
+                if(get_poids_total(g, checked, chemin_test)<get_poids_total(g, checked, liste_ville)){
+                    copieTable(liste_ville, chemin_test, n);
+                    //printf("nouveau meilleur chemin trouvé!\n");
+                }
+            }
+            //printf("Nombre de villes dans la liste : %d\n", checked);
+            //afficheCheminPoids(g, n, liste_ville);
+            //checked++;
+        }
+}
+
+
+/*
+//p : position d'insertion
+//x : valeur à inserer
+//n : longueur de la table (pas forcément la longueur maximal de la table, juste la longueur concernée)
+void insererDansTable(int p, int x, int n, int *liste_ville){
+    for (int i=n; i>p; i--){
+        
+    }
+    
+}*/
+
+
+
+void decalageVersGauche(int *table, int n){
+        int save=table[0];
+      for(int i=0; i<n; i++){
+        table[i]=table[i+1];
+      }
+      table[n-1]=save;
+}
+
+void insererElementDroite(int decalage, int rangDep, int *liste){
+    for (int i=rangDep; i>rangDep-decalage; i--){
+        decalageVersGauche(&liste[i], 2);
+    }
+}
+
+
+
+
+
+int get_poids_total(Graphe g, int n, int *ordre_ville){
+	int somme_poids = 0;
+	int point_a, point_b;
+    
+	for (int i = 0; i < n-1; i++){
+		point_a = ordre_ville[i];
+		point_b = ordre_ville[i+1];	
+		somme_poids += g[point_a][point_b];
+	}
+
+	return somme_poids;
+}
