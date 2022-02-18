37a95b4613db3c0dab349ecb5c603a0f32eb6008
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-02-18 18:21:47 +0100

    Update table.c

--- Fonction/table.c
+++ Fonction/table.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Copie le tableau B dans le tableau A, de taile size
+// Copie le tableau B dans le tableau A, de taille size
 void copieTable(int a[], int b[], int size){
     for (int i = 0; i < size; i++){
         a[i] = b[i];
