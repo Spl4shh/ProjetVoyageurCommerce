b75c7e019bde49ef3f2933aabebaa4e5db794965
Romain Natanelic <74205736+Spl4shh@users.noreply.github.com>
2022-02-21 14:54:06 +0100

    recherche du plus proche voisin sous forme de fonction
    recherche pour tout les points de depart existant

--- heuristique_3/heuristique3_2.c
+++ heuristique_3/heuristique3_2.c
@@ -8,8 +8,15 @@
 #include "../Programme_Fourni/calculTemps.c"
 
 #include "../Fonction/poids.c"
+#include "../Fonction/table.c"
 #include "../Fonction/recherche.c"
 #include "../Fonction/affichageCombine.c"
+
+/*
+	Cet fonction permet de construire le tableau ordre_ville afin d'avoir le chemin
+	construit en fonction du plus proche voisin.
+*/
+void rechercheCheminVoisinPlusProche(Graphe G, int n, int ordre_ville[]);
 
 int main(int argc, char const *argv[])
 {
@@ -18,9 +25,7 @@ int main(int argc, char const *argv[])
 	Graphe G = NULL;
 	int n, m;
 	int err;
-	int ville_actuelle = 0, ville_suivante;
 	clock_t timer;
-	srand((unsigned int)time(NULL));
       //End Variable
 
       //lecture du fichier
@@ -31,40 +36,74 @@ int main(int argc, char const *argv[])
 		err = lire_data(nom, &G, &n, &m);
 	}while(err == 0);
 
-	/*
-	Liste_ville sert a verifier les villes dans lesquelles on est deja passé
-	ordre_ville sert a sauvegarder l'ordre dans lequel on est passé
-	*/
-	int *liste_ville = NULL, *ordre_ville = NULL;
+	
+	// ordre_ville sert a sauvegarder le chemin optimal
+	int *ordre_ville = NULL;
 
-	liste_ville = malloc(n * sizeof(int));
 	ordre_ville = malloc(n * sizeof(int));
-
-	liste_ville[ville_actuelle] = 1;
-	ordre_ville[0] = ville_actuelle;
 
 	// Declenche l'horloge
 	timer = clock();
 
-	for(int i = 0; i < n; i++)
-	{	
-		ville_suivante = rechercheVoisinProche(G, n, ville_actuelle, liste_ville);
-		
-		//On rentre ici seulement si l'on a pas exploré toutes les villes
-		if (ville_suivante != -1)
-		{
-			ville_actuelle = ville_suivante;
-			liste_ville[ville_actuelle] = 1;
-			ordre_ville[i+1] = ville_actuelle;;
-		}
-	}
+	rechercheCheminVoisinPlusProche(G, n, ordre_ville);
 	
-	// Calcul distance totale
-	int distance_totale = getPoidsTotal(G, n, ordre_ville);
-
 	// Affichage resultat
-	afficheCheminPoids(G, n, ordre_ville);
 	affichageTimer(timer);
+	afficheCheminPoids(G, n, ordre_ville);
 	
       return 0;
+}
+
+void rechercheCheminVoisinPlusProche(Graphe G, int n, int ordre_ville[]){
+	// Variables
+	int ville_actuelle, ville_suivante;
+	int firstAssign = 1;
+	int *liste_ville = NULL, *new_ordre_ville = NULL;
+
+	for (int i = 0; i < n; i++){
+		
+		liste_ville = malloc(n * sizeof(int));
+		new_ordre_ville = malloc(n * sizeof(int));
+
+		// Initialise le tableau de la liste des villes visitée
+		for (int h = 0; h < n; h++){
+			liste_ville[h] = 0;
+		}
+		
+		ville_actuelle = i;
+		liste_ville[ville_actuelle] = 1;
+		new_ordre_ville[0] = ville_actuelle;
+
+		/*
+			Construit pour le sommet de depart actuel 
+			le chemin a partir des plus proche voisin
+		*/
+		for(int j = 0; j < n; j++){	
+			ville_suivante = rechercheVoisinProche(G, n, ville_actuelle, liste_ville);
+
+			//On rentre ici seulement si l'on a pas exploré toutes les villes
+			if (ville_suivante != -1){
+				ville_actuelle = ville_suivante;
+				liste_ville[ville_actuelle] = 1;
+				new_ordre_ville[j+1] = ville_actuelle;;
+			}
+		}
+
+		/*
+			Assigne l'ordre de ville testé si c'est le premier test
+			Ou si le poids du nouveau chemin est plus faible que l'ancien
+		*/
+		if (!firstAssign){
+			if (getPoidsTotal(G, n, ordre_ville) > getPoidsTotal(G, n, new_ordre_ville)){
+				copieTable(ordre_ville, new_ordre_ville, n);
+			}
+		}else{
+			copieTable(ordre_ville, new_ordre_ville, n);
+			firstAssign = 0;
+		}
+
+		// Libere l'espace (et donc le contenu) alloué aux tableaux
+		free(liste_ville);
+		free(new_ordre_ville);
+	}
 }
