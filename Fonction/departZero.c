6bb12403b31fed49359b7100abc9b6d752ea3bdc
LothaireNobili <91308048+LothaireNobili@users.noreply.github.com>
2022-02-22 09:50:02 +0100

    prise en paramètre du nombre recherch&

--- Fonction/departZero.c
+++ Fonction/departZero.c
@@ -1,15 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void demarrerZero(int *table, int n){
+void demarrerZero(int *table, int n, int r){
+
     int save;
-    while(table[0]!=0){
+    int c = 0;
+
+    while(table[0] != r && c < n){
+        c++;
         printf("debug\n");
+
         save=table[0];  //on stock la donnée qui va être écrasée dans une variable
-            for(int i=0; i<n; i++){
-                table[i]=table[i+1];
+            for(int i = 0; i < n; i++){
+                table[i] = table[i+1];
             }
-        table[n-1]=save;  //on ressort la donnée precedemment écrasée
+
+        table[n-1] = save;  //on ressort la donnée precedemment écrasée
+
+    }
+
+    if (c >= n){
+        printf("La valeur recherchée n'a pas été trouvée");
     }
     
 }
