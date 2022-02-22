a4286ae40cce80eb4be06e0776ee3650aec30981
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-02-22 14:05:08 +0100

    corrrect

--- Fonction/departZero.c
+++ Fonction/departZero.c
@@ -1,28 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
-#ifndef DEMARRERZERO_C
-	#define DEMARRERZERO_C
+#ifndef DEPARTZERO_H
+#define DEPARTZERO_H
 
-    void demarrerZero(int *table, int n, int r){
+void demarrerZero(int *table, int n, int r){
+    int save;
+    int c = 0;
 
-        int save;
-        int c = 0;
+    while(table[0] != r && c < n){
+        c++;
 
-        while(table[0] != r && c < n){
-            c++;
-
-            save=table[0];  //on stock la donnée qui va être écrasée dans une variable
-                for(int i = 0; i < n; i++){
-                    table[i] = table[i+1];
-                }
-
-            table[n-1] = save;  //on ressort la donnée precedemment écrasée
-
+        save=table[0];  // On stock la donnée qui va être écrasée dans une variable
+        for(int i = 0; i < n; i++){
+            table[i] = table[i+1];
         }
 
-        if (c >= n){
-            printf("La valeur recherchée n'a pas été trouvée");
-        }
-        
+        table[n-1] = save;  //on ressort la donnée precedemment écrasée
     }
+
+    if (c >= n){
+        printf("La valeur recherchée n'a pas été trouvée");
+    }   
+}
+
 #endif
--- Fonction/departZero.h
+++ Fonction/departZero.h
@@ -1,5 +1,6 @@
-#ifndef DEMARRERZERO_H
-      #define DEMARRERZERO_H
+#ifndef DEPARTZERO_H
+      #define DEPARTZERO_H
+      
       //place r en position 0 de la table
       void demarrerZero(int *table, int n, int r)
 #endif
