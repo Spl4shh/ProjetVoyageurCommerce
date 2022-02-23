2cfae3a4e647a87d1b86feb6456f40f34e17d5c5
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2022-02-23 15:40:34 +0100

    clearing spaces

--- force_brute/permutBrute.c
+++ force_brute/permutBrute.c
@@ -2,17 +2,18 @@
 #include <time.h>
 #include <stdlib.h>
 
-int prochainePermut(int a[], int len) {
+int brutePermut(int a[], int len) {
 
     // Find non-increasing suffix
     if (len == 0) {
         return 0;
     }
     int i = len - 1;
-    while (i > 0 && a[i - 1] >= a[i]) {
+
+    while (i > 0 && a[i - 1] >= a[i]){
         i--;
     }
-    if (i == 0) {
+    if (i == 0){
         return 0;
     }
     
@@ -42,106 +43,35 @@ int prochainePermut(int a[], int len) {
     return 1;
 }
 
-void quicksort( int a[], int start, int end ) {
-
+void quicksort(int a[], int start, int end){
     int i, j, pivot, temp;
 
     if (start < end ) {
-
         pivot = start;
-
         i = start ;
-
         j = end;
 
         while( i < j ) {
-
             while(a[i] <= a[pivot] && i < end) {
-
                 i++;
             }
 
-            while( a[j] > a[pivot]) {
-
+            while( a[j] > a[pivot]){
                 j--;
             }
 
-            if( i < j ) {
-
+            if( i < j ){
                 temp = a[i];
-
                 a[i] = a[j];
-
                 a[j] = temp;
-
             }
-
         }
 
         temp = a[pivot];
-
         a[pivot] = a[j];
-
         a[j] = temp;
 
         quicksort(a,start,j-1);
-
         quicksort(a,j+1,end);
-
     }
-
 }
-
-/*
-void brutePermut( int a[], int len ) {
-
-    quicksort(a, 0, len - 1);
-
-    printf("Permutations possible sont : \n");
-
-    do {
-        for ( int i = 0; i < len; i ++ ) {
-            printf("%d ", a[i]);
-        }
-        printf("\n");
-    } while ( prochainePermut(a, len));
-}
-*/
-
-void brutePermut( int a[], int len ) {
-   
-   
-   /*
-    if ( prochainePermut(a, len)) {
-        for ( int i = 0; i < len; i ++ ) {
-            printf("%d ", a[i]);
-        }
-        printf("\n");
-    }
-    */
-    
-    prochainePermut(a, len);
-}
-
-/*
-int main() {
-
-   int tab[] = {1,2,3,4};
-   int len = sizeof(tab) / sizeof(tab[0]);
-   int i;
-   
-   quicksort(tab, 0, len - 1);
-   
-   int fact = 1;
-   for (i = 1; i <= len; i++) {
-      fact = fact * i;
-   }
-   
-   for ( i = 0; i < fact; i++ ) {
-   
-      brutePermut(tab, len);
-      
-   }
-
-}
-*/
